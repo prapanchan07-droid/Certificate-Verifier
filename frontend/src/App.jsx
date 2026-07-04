57dae50ba3e1f09684d40997436118c04382dd8f
prapanchan07-droid <prapanchan07@gmail.com>
2026-07-05 00:57:51 +0530

    updated useEffect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import FileUploader from "./components/FileUploader";
 import VerificationMetrics from "./components/VerificationMetrics";
 import Dashboard from "./components/Dashboard";
