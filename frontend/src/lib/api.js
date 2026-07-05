4b6519a4b7a55f86495465d5c9833b3f6b4e0ded
prapanchan07-droid <prapanchan07@gmail.com>
2026-07-05 16:38:55 +0530

    Application

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = "https://certificate-verifier-dayu.onrender.com";
+const API_BASE_URL = "http://3.91.58.234:8000";
+// const API_BASE_URL = "https://certificate-verifier-dayu.onrender.com";
 
 export async function verifyCertificate(file) {
   const formData = new FormData();
